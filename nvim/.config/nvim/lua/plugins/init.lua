430a58d37f73e2142c964346447b5881eaff8229
Tu Vu <tuvu852@gmail.com>
2021-12-27 12:21:01 -0600

    Update treesitter config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,4 @@
 require("plugins.packer")
 require("plugins.autopairs")
 require("plugins.cmp")
+require("plugins.treesitter")
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained",
-	sync_install = false, -- Install languages synchronously (only applied to `ensure_installed`)
 	highlight = { enable = true },
 
 	-- plugins
 	autopairs = { enable = true },
 	autotag = {
 		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
 	},
 })
