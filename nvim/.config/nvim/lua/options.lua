6a132951fc3c47eb45b0a249cdb0237300c3f01f
Tu Vu <tuvu852@gmail.com>
2021-12-28 10:22:33 -0600

    Revert to 4 spaces indentation

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,15 +1,14 @@
 local set = vim.opt
-local setlocal = vim.opt_local
 
 set.relativenumber = true -- Set relative number = offset distance from current line
 set.number = true -- Show current line number
-set.tabstop = 2 -- Number of columns occupied by a tab
-set.softtabstop = 2 -- Set multiple spaces as tabstops
+set.tabstop = 4 -- Number of columns occupied by a tab
+set.softtabstop = 4 -- Set multiple spaces as tabstops
 set.expandtab = true -- Converts tabs to white space
 set.autoindent = true -- Indent the new line same level as previous line
-set.shiftwidth = 2 -- Width for autoindent
+set.shiftwidth = 4 -- Width for autoindent
 set.clipboard = "unnamedplus" -- Using system clipboard
 set.incsearch = true -- Incremental highlight while searching
-setlocal.wrap = false -- No wrap line
+set.wrap = false -- No wrap line
 set.swapfile = false -- Disable creating new swap file
 set.scrolloff = 10 -- Keeping the cursor in the center while scrolling
