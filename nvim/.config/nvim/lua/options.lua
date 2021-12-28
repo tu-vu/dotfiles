394eb295fc48479413d3a61e1d3271d935374b28
Tu Vu <tuvu852@gmail.com>
2021-12-27 19:21:33 -0600

    Change to 2 spaces indentation

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,11 +3,11 @@ local setlocal = vim.opt_local
 
 set.relativenumber = true -- Set relative number = offset distance from current line
 set.number = true -- Show current line number
-set.tabstop = 4 -- Number of columns occupied by a tab
-set.softtabstop = 4 -- Set multiple spaces as tabstops
+set.tabstop = 2 -- Number of columns occupied by a tab
+set.softtabstop = 2 -- Set multiple spaces as tabstops
 set.expandtab = true -- Converts tabs to white space
 set.autoindent = true -- Indent the new line same level as previous line
-set.shiftwidth = 4 -- Width for autoindent
+set.shiftwidth = 2 -- Width for autoindent
 set.clipboard = "unnamedplus" -- Using system clipboard
 set.incsearch = true -- Incremental highlight while searching
 setlocal.wrap = false -- No wrap line
