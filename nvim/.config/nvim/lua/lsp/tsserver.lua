2fee92cb1ac59e9d601abec8141dddc73a292051
Tu Vu <tuvu852@gmail.com>
2021-12-29 20:03:57 -0600

    Add auto import utility

--- nvim/.config/nvim/lua/lsp/tsserver.lua
+++ nvim/.config/nvim/lua/lsp/tsserver.lua
@@ -3,17 +3,32 @@ if not status_ok then
 	return
 end
 
+local u = require("utils")
+
+local ts_utils = require("nvim-lsp-ts-utils")
+local ts_utils_settings = {
+	enable_import_on_completion = true,
+	update_imports_on_move = true,
+	-- filter out dumb module warning
+	filter_out_diagnostics_by_code = { 80001 },
+}
+
 local M = {}
 M.setup = function(on_attach, capabilities)
 	lspconfig.tsserver.setup({
-		on_attach = function(client, buffer)
-			client.resolved_capabilities.document_formatting = false
-			client.resolved_capabilities.document_range_formatting = false
+		root_dir = lspconfig.util.root_pattern("package.json"),
+		init_options = ts_utils.init_options,
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
 
-			on_attach(client, buffer)
+			ts_utils.setup(ts_utils_settings)
+			ts_utils.setup_client(client)
+
+			u.buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
+			u.buf_map(bufnr, "n", "gI", ":TSLspRenameFile<CR>")
+			u.buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
 		end,
 		capabilities = capabilities,
-		filetypes = {},
 	})
 end
 
