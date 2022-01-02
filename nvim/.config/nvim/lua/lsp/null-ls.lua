12dc7ecf67df1f8967876f79dc257a4d3c4dc484
Tu Vu <tuvu852@gmail.com>
2022-01-02 16:40:00 -0600

    Back to eslint_d and prettierd...
    This reverts commit 3cc05b5462c7905173591ec5d72674113018c1cd.

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -13,9 +13,9 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	diagnostics.eslint,
-	code_actions.eslint,
-	formatting.prettier,
+	diagnostics.eslint_d,
+	code_actions.eslint_d,
+	formatting.prettierd,
 	formatting.stylua,
 }
 
