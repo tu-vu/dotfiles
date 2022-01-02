3cc05b5462c7905173591ec5d72674113018c1cd
Tu Vu <tuvu852@gmail.com>
2022-01-02 16:11:15 -0600

    Switch to eslint and prettier

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -13,9 +13,9 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	diagnostics.eslint_d,
-	code_actions.eslint_d,
-	formatting.prettierd,
+	diagnostics.eslint,
+	code_actions.eslint,
+	formatting.prettier,
 	formatting.stylua,
 }
 
